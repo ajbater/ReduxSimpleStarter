c60f67e6888b42bad4916d92a80213024cc1f1e5
Amanda Bater <amanda22@hotmail.co.uk>
2017-09-11 18:03:57 +0100

    Creates action for fetching posts, installs redux-promise and axios

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
 import { BrowserRouter, Route } from "react-router-dom";
+import reduxPromise from "redux-promise";
 
 import reducers from "./reducers";
 import PostsIndex from "./components/PostsIndex";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
