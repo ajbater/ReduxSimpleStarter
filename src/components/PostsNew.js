0750c1966fae238f7aaa92fa65da000222a22503
Amanda Bater <amanda22@hotmail.co.uk>
2017-09-12 11:34:12 +0100

    Creates cancel new post button

--- src/components/PostsNew.js
+++ src/components/PostsNew.js
@@ -1,11 +1,70 @@
 import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { Link } from "react-router-dom";
 
-export default class PostsNew extends Component {
-  render() {
+class PostsNew extends Component {
+  renderField(field) {
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`
     return (
-      <div>
-        
+      <div className={className}>
+        <label>{field.label}</label>
+        <input
+          type="text"
+          className="form-control"
+          {...field.input}
+        />
+        <div className="text-help">{touched ? error : ""}</div>
       </div>
     );
   }
+
+  onSubmit(values) {
+    console.log(values);
+  }
+
+  render() {
+    const { handleSubmit } = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
+        <Field
+          name="title"
+          label="Title"
+          component={this.renderField}
+        />
+        <Field
+          name="tags"
+          label="Tags"
+          component={this.renderField}
+        />
+        <Field
+          name="content"
+          label="Post Content"
+          component={this.renderField}
+        />
+        <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
+      </form>
+    );
+  }
 }
+
+function validate(values) {
+  const errors = {};
+  if (!values.title) {
+    errors.title = "Please enter a title";
+  }
+  if (!values.tags) {
+    errors.tags = "Please add some tags";
+  }
+  if (!values.content) {
+    errors.content = "Please add some content to the post";
+  }
+  return errors;
+}
+
+export default reduxForm({
+  validate,
+  form: "PostsNewForm"
+})(PostsNew);
