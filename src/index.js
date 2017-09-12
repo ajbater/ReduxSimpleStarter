549154c47418b3b47dfd9475861fddbad20c6ff1
Amanda Bater <amanda22@hotmail.co.uk>
2017-09-12 09:57:26 +0100

    Creates PostsNew component and route

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import reduxPromise from "redux-promise";
 
 import reducers from "./reducers";
 import PostsIndexContainer from "./containers/PostsIndexContainer";
+import PostsNew from "./components/PostsNew";
 
 const createStoreWithMiddleware = applyMiddleware(reduxPromise)(createStore);
 
@@ -14,7 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndexContainer} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndexContainer} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
